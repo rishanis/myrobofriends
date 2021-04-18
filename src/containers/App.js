dcebf8e09c27f801140dd7ea888804adb4d461d4
rishanis <rishanis@outlook.com>
2021-04-18 23:47:53 +0400

    tittle changed

--- src/containers/App.js
+++ src/containers/App.js
@@ -33,7 +33,7 @@ class App extends Component {
     <h1 className='tc'>Loading...</h1> :
     (
       <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
+        <h1 className='f1'>My Robo Friends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
         <Scroll>
           <ErrorBoundry>
